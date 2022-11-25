e504bf377afeef8eecd74fae3262c9de48b2b413
Jon Barker <j.p.barker@sheffield.ac.uk>
2022-11-25 16:15:36 +0000

    Config for netlify build

--- docusaurus-netlify.config.js
+++ docusaurus-netlify.config.js
@@ -1,93 +1,158 @@
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 const math = require('remark-math');
 const katex = require('rehype-katex');
+const {
+    MailRounded
+} = require('@mui/icons-material');
+
 module.exports = {
-    title: 'The 2nd Clarity Enhancement Challenge',
-    tagline: 'A machine learning challenge for hearing aid processing',
-    url: 'https://claritychallenge.org',
+    title: 'The Clarity Project',
+    tagline: 'Organising machine learning challenges for hearing aid processing',
+    url: 'https://claritychallenge.github.io/',
     baseUrl: '/',
-    onBrokenLinks: 'error',
-    onBrokenMarkdownLinks: 'error',
+    onBrokenLinks: 'throw',
+    onBrokenMarkdownLinks: 'throw',
     favicon: 'img/favicon.ico',
     organizationName: 'claritychallenge', // Usually your GitHub org/user name.
-    projectName: 'clarity_CC_doc', // Usually your repo name.
+    projectName: 'claritychallenge.github.io', // Usually your repo name.
+    /* trailingSlash: false, */
+    deploymentBranch: "gh-pages",
     stylesheets: [{
         href: 'https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.css',
         type: 'text/css',
         integrity: 'sha384-AfEj0r4/OFrOo5t7NnNe46zW/tFgW6x/bCJG8FqQCEo3+Aro6EYUG4+cU+KJWu/X',
         crossorigin: 'anonymous',
     }, "https://fonts.googleapis.com/icon?family=Material+Icons", ],
-    plugins: [require.resolve('docusaurus-lunr-search'), './sitePlugin'],
+    plugins: [
+        [require.resolve('docusaurus-lunr-search'), {
+            indexBaseUrl: true
+        }], './sitePlugin', [
+            '@docusaurus/plugin-client-redirects',
+            {
+                redirects: [{
+                    to: '/',
+                    from: '/clarity_CC_doc',
+                }, ],
+            },
+        ], require.resolve("docusaurus-plugin-image-zoom")
+    ],
     themeConfig: {
         navbar: {
             title: 'Clarity',
+            hideOnScroll: true,
             logo: {
                 alt: 'Clarity Logo',
                 src: 'img/logo.png',
             },
-            items: [{
-                    type: 'doc',
-                    docId: 'cec2/cec2_intro',
-                    position: 'left',
-                    label: 'CEC2 Intro',
-                }, {
-                    type: 'doc',
-                    docId: 'cec2/cec2_dates',
-                    position: 'left',
-                    label: 'Dates',
-                },
-                {
-                    type: 'doc',
-                    docId: 'cec2/cec2_download',
-                    position: 'left',
-                    label: 'Download',
-                },
-                {
-                    type: 'doc',
-                    docId: 'cec2/cec2_submission',
-                    position: 'left',
-                    label: 'Submission',
-                },
+            items: [
 
                 {
                     type: 'dropdown',
-                    label: 'Previous Workshops',
-                    position: 'right',
+                    label: 'CEC2 Shortcuts',
+                    position: 'left',
                     items: [{
-                        href: 'https://claritychallenge.github.io/clarity2021-workshop/',
-                        label: 'Clarity 2021',
-                    }]
+                            type: 'doc',
+                            docId: 'cec2/cec2_intro',
+                            label: 'I want to learn more sbout CEC2...',
+                        },
+                        {
+                            type: 'doc',
+                            docId: 'cec2/cec2_download',
+                            label: 'I want to download the data...',
+                        },
+                        {
+                            href: 'https://github.com/claritychallenge/clarity',
+                            label: 'I want to see the code on GitHub...',
+                        },
+                        {
+                            type: 'doc',
+                            docId: 'cec2/taking_part/cec2_registration',
+                            label: 'I want to register a team...',
+                        },
+                        {
+                            type: 'doc',
+                            docId: 'cec2/taking_part/cec2_submission',
+                            label: 'I want to submit results...',
+                        },
+
+                    ]
                 },
                 {
                     type: 'dropdown',
-                    label: 'Previous Challenges',
+                    label: 'Workshops',
+                    position: 'right',
+                    items: [{
+                            href: 'https://claritychallenge.github.io/clarity2022-CEC2-workshop/',
+                            label: 'Clarity CEC2 2022, Dec',
+                        }, {
+                            href: 'https://claritychallenge.github.io/clarity2022-workshop/',
+                            label: 'Clarity 2022, Jun',
+                        }, {
+                            href: 'https://claritychallenge.github.io/clarity2021-workshop/',
+                            label: 'Clarity 2021',
+                        }
+
+                    ]
+                },
+                {
+                    type: 'dropdown',
+                    label: 'Challenges',
                     position: 'right',
                     items: [{
                         type: 'doc',
-                        docId: 'cec1/intro',
+                        docId: 'cec2/cec2_intro',
+                        label: 'CEC2 (current)',
+                    }, {
+                        type: 'doc',
+                        docId: 'cpc1/cpc1_intro',
+                        label: 'CPC1',
+                    }, {
+                        type: 'doc',
+                        docId: 'cec1/cec1_intro',
                         label: 'CEC1',
                     }, {
                         type: 'doc',
-                        docId: 'cpc1/intro',
-                        label: 'CPC1',
+                        docId: 'icassp_2023/icassp2023_intro',
+                        label: 'ICASSP 2023 Grand Challenge',
+                    }, {
+                        to: '/timeline',
+                        label: 'Future Challenges',
                     }, ]
                 },
 
                 {
-                    type: 'doc',
-                    docId: 'contact',
-                    label: 'Contact',
-                    position: 'right'
+                    type: 'dropdown',
+                    label: 'Software',
+                    position: 'right',
+                    items: [{
+                        to: 'tutorials',
+                        label: 'Tutorials',
+                    }, {
+                        href: 'https://github.com/claritychallenge/clarity',
+                        label: 'GitHub',
+                    }, ]
+                },
+                {
+                    type: 'dropdown',
+                    label: 'About Us',
+                    position: 'right',
+                    items: [{
+                        to: '/about/',
+                        label: 'About Us',
+                    }, {
+                        to: '/contact/',
+                        label: 'Contact Us',
+
+                    }, {
+                        to: '/timeline',
+                        label: 'Project timeline',
+                    }, ]
                 }, {
-                    to: '/blog',
+                    to: '/blog/',
                     label: 'Blog',
                     position: 'right'
                 },
-                {
-                    href: 'https://github.com/claritychallenge/clarity_CEC1',
-                    label: 'GitHub',
-                    position: 'right',
-                },
+
             ],
         },
         footer: {
@@ -95,8 +160,17 @@ module.exports = {
             links: [{
                     title: 'Docs',
                     items: [{
+                        label: 'CEC2 Documentation',
+                        to: '/docs/cec2/cec2_intro/',
+                    }, {
+                        label: 'CPC1 Documentation',
+                        to: '/docs/cpc1/cpc1_intro/',
+                    }, {
                         label: 'CEC1 Documentation',
-                        to: '/docs/cec1/intro',
+                        to: '/docs/cec1/cec1_intro/',
+                    }, {
+                        label: 'CASSP 2023 Documentation',
+                        to: '/docs/icassp_2023/icassp2023_intro/',
                     }, ],
                 },
                 {
@@ -109,23 +183,37 @@ module.exports = {
                             label: 'Clarity Google Group',
                             href: 'https://groups.google.com/g/clarity-challenge',
                         },
+                        {
+                            label: 'Email Us',
+                            href: 'mailto:claritychallengecontact@gmail.com'
+                        }
                     ],
                 },
                 {
                     title: 'More',
                     items: [{
                             label: 'Challenge Updates',
-                            to: '/blog',
+                            to: '/blog/',
                         },
                         {
                             label: 'GitHub',
-                            href: 'https://github.com/claritychallenge/clarity_CEC1',
-                        },
+                            href: 'https://github.com/claritychallenge/clarity',
+                        }
                     ],
                 },
             ],
             copyright: `Copyright © ${new Date().getFullYear()} The Clarity Team. Built with Docusaurus.`,
         },
+        zoom: {
+            selector: '.markdown img',
+            config: {
+                // options you can specify via https://github.com/francoischalifour/medium-zoom#usage
+                background: {
+                    light: 'rgb(255, 255, 255)',
+                    dark: 'rgb(50, 50, 50)'
+                }
+            }
+        }
     },
     presets: [
         [
@@ -138,19 +226,22 @@ module.exports = {
                 },
                 docs: {
                     sidebarPath: require.resolve('./sidebars.js'),
-                    remarkPlugins: [math],
+                    remarkPlugins: [require('mdx-mermaid'), math],
                     rehypePlugins: [
                         [katex, {
-                            stict: false
+                            strict: false
                         }]
                     ],
                     // Please change this to your repo.
-                    editUrl: 'https://github.com/claritychallenge/clarity_CC_doc/edit/master/',
                 },
                 blog: {
                     showReadingTime: true,
                     // Please change this to your repo.
-                    editUrl: 'https://github.com/claritychallenge/clarity_CC_doc/edit/master/',
+                    blogTitle: 'Clarity Blog',
+                    blogDescription: 'News and updates about our Challenges and Workshops',
+                    postsPerPage: 'ALL',
+                    blogSidebarTitle: 'Recent posts',
+                    blogSidebarCount: 10,
                 },
                 theme: {
                     customCss: require.resolve('./src/css/custom.css'),
