bb5eaaeea9dee11d4af6dbc0db490c210f773d48
Jon Barker <j.p.barker@sheffield.ac.uk>
2023-11-01 00:20:13 +0000

    Reorganised pages to make challenge structures more similar

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -72,7 +72,7 @@ module.exports = {
                         },
                         {
                             type: 'doc',
-                            docId: 'cpc2/taking_part/cpc2_download',
+                            docId: 'cpc2/cpc2_download',
                             label: 'I want to download the data...',
                         },
                         {
